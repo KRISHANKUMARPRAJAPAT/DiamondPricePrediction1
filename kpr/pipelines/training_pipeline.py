af72adc44571870f3db113b77c5c2ebaf8a7d994
KRISHNA KUMAR PRAJAPAT <krishnakumarprajapat366@gmail.com>
2024-02-03 14:27:23 +0530

    push into repository

---
+++ kpr/pipelines/training_pipeline.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from kpr.logger import logging
+from kpr.exception import CustomException
+import pandas as pd
+
+from kpr.components.data_ingestion import DataIngestion
+
+from kpr.components.data_transformation import DataTransformation
+from kpr.components.model_trainer import ModelTrainer
+
+
+
+if __name__=='__main__':
+    obj=DataIngestion()
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
+    print(train_data_path,test_data_path)
+
+    data_transformation=DataTransformation()
+
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+
+
+# for model trainer file 
+    model_trainer=ModelTrainer()
+    model_trainer.initate_model_training(train_arr,test_arr)
+
